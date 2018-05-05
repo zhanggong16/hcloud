423c7aed00301f6a29d479790dcd507f133f01ff
machao96 <machao31@jd.com>
2018-05-05 11:06:08 +0800

    提交报警相关功能

--- docs/url/alert_rules_add.py
+++ docs/url/alert_rules_add.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 import requests
 
-hostlist = 'http://localhost:5000/api/v1/alert_rules'
+alert_rules = 'http://localhost:6000/api/v1/alert_rules'
 
 
 payload = {
-  "host_id": "192.168.0.92",
+  "host_id": "192.168.0.93",
   "port": 9100,
   "service": "node",
   "monitor_items":  "memory_usage",
-  "statistical_period": "5m",
+  "statistical_period": "5s",
   "statistical_approach":  "max",
   "compute_mode":  ">",
-  "threshold_value": 80
+  "threshold_value": 1,
+  "contact_groups":"machao",
+  "notify_type": 0
 }
 
-r = requests.post(hostlist, params=payload)
+r = requests.post(alert_rules, params=payload)
 
 rs = r.json()
 
 print rs
 print r.status_code
+
--- hcloud/utils.py
+++ hcloud/utils.py
@@ -1,5 +1,6 @@
 import os
 from eventlet.green import subprocess
+import shlex
 
 def cmd_run(cmd):
     _pipe = subprocess.PIPE
@@ -12,3 +13,13 @@ def cmd_run(cmd):
     obj.stdin.close()
     _returncode = obj.returncode
     return _returncode, result
+
+def execute_command(cmdstring, cwd=None, preexec_fn=None, env=None, shell=False):
+    if shell:
+        cmdstring_list = cmdstring
+    else:
+        cmdstring_list = shlex.split(cmdstring)
+
+    sub = subprocess.Popen(cmdstring_list, cwd=cwd, preexec_fn=preexec_fn, env=env, stdin=subprocess.PIPE,stdout=subprocess.PIPE, shell=shell, bufsize=4096)
+    output, err = sub.communicate()
+    return int(sub.returncode), output, err
